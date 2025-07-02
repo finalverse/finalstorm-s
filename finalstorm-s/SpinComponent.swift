9471c3d19384bf55ccf48e519f4f8d1ba72328f9
qwy16 <qwy@msn.com>
2025-07-02 15:01:27 +0800

    Initial Commit

---
+++ finalstorm-s/SpinComponent.swift
@@ -0,0 +1,13 @@
+//
+//  SpinComponent.swift
+//  finalstorm-s
+//
+//  Created by Wenyan Qin on 2025-07-02.
+//
+
+import RealityKit
+
+/// A component that spins the entity around a given axis.
+struct SpinComponent: Component {
+    let spinAxis: SIMD3<Float> = [0, 1, 0]
+}
---
+++ finalstorm-s/finalstorm_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalstorm_sApp.swift
+//  finalstorm-s
+//
+//  Created by Wenyan Qin on 2025-07-02.
+//
+
+import SwiftUI
+
+@main
+struct finalstorm_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
